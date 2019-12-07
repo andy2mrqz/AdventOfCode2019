c283ab23efd5d77c6980a875f08ed030a6ad4fcc
Andrew Marquez <andy2mgcc@gmail.com>
2019-12-07 16:43:06 -0700

    Refactored day 4 part 1 for readability

--- Day04/Day04p1.hs
+++ Day04/Day04p1.hs
@@ -4,10 +4,18 @@ import Data.Char (digitToInt)
 
 input = map show [278384..824795]
 
+isOrdered :: [Int] -> Bool
+isOrdered [x] = True
+isOrdered (x:y:xs) = x <= y && isOrdered(y:xs)
+
+hasAdj :: String -> Bool
+hasAdj (_:[]) = False
+hasAdj (x:xs) = (x == head xs) || hasAdj xs
+
 criteria :: String -> Bool
 criteria pw =
     length pw == 6 &&
-    (snd $ foldl (\(prev, adj) n -> (n, adj || (prev == n))) ('x', False) pw) && 
-    (snd $ foldl (\(prev, incr) n -> (n, incr && (digitToInt prev <= digitToInt n))) ('0', True) pw)
+    hasAdj pw &&
+    (isOrdered $ map digitToInt pw)
 
 main = print $ length $ filter criteria input
