5119c99b0530796fa529affa59092efc6898ffd6
Andrew Marquez <andy2mgcc@gmail.com>
2019-12-07 19:29:33 -0700

    Finished day 4 part 2

---
+++ Day04/Day04p2.hs
@@ -0,0 +1,18 @@
+module Main where
+
+import Data.Char (digitToInt)
+import qualified Data.List as L
+
+input = map show [278384..824795]
+
+isOrdered :: [Int] -> Bool
+isOrdered [x] = True
+isOrdered (x:y:xs) = x <= y && isOrdered(y:xs)
+
+criteria :: String -> Bool
+criteria pw =
+    length pw == 6 &&
+    (any (\g -> (length g) == 2) $ L.group pw) &&
+    (isOrdered $ map digitToInt pw)
+
+main = print $ length $ filter criteria input
