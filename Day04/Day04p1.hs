3f39d7eebb0b79566c219c9da93848400d1701c7
Andrew Marquez <andy2mgcc@gmail.com>
2019-12-07 19:28:12 -0700

    Refactored day 4 part 1 with better technique

--- Day04/Day04p1.hs
+++ Day04/Day04p1.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Data.Char (digitToInt)
+import qualified Data.List as L
 
 input = map show [278384..824795]
 
@@ -8,16 +9,10 @@ isOrdered :: [Int] -> Bool
 isOrdered [x] = True
 isOrdered (x:y:xs) = x <= y && isOrdered(y:xs)
 
-eqlTpl :: (Char, Char) -> Bool
-eqlTpl (a,b) = a == b
-
-stagger :: [a] -> [(a, a)]
-stagger str = zip str (tail str)
-
 criteria :: String -> Bool
 criteria pw =
     length pw == 6 &&
-    (any eqlTpl $ stagger pw) &&
+    (any (\g -> (length g) > 1) $ L.group pw) &&
     (isOrdered $ map digitToInt pw)
 
 main = print $ length $ filter criteria input
