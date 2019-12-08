268cac1d675d42c0df492f6d611296ae9ff72eab
Andrew Marquez <andy2mgcc@gmail.com>
2019-12-07 19:33:46 -0700

    Refactored day 4 with generic sort check

--- Day04/Day04p2.hs
+++ Day04/Day04p2.hs
@@ -1,11 +1,10 @@
 module Main where
 
-import Data.Char (digitToInt)
 import qualified Data.List as L
 
 input = map show [278384..824795]
 
-isOrdered :: [Int] -> Bool
+isOrdered :: Ord a => [a] -> Bool
 isOrdered [x] = True
 isOrdered (x:y:xs) = x <= y && isOrdered(y:xs)
 
@@ -13,6 +12,6 @@ criteria :: String -> Bool
 criteria pw =
     length pw == 6 &&
     (any (\g -> (length g) == 2) $ L.group pw) &&
-    (isOrdered $ map digitToInt pw)
+    (isOrdered pw)
 
 main = print $ length $ filter criteria input
